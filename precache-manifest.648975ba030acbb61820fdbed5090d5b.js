d9c9b7323ad004afba50704e2d0815c43cda625b
Kbadr1 <kbadr.dev@gmail.com>
2020-08-07 20:30:02 +0200

    Updates

---
+++ precache-manifest.648975ba030acbb61820fdbed5090d5b.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5d794fc74f0b5a372596c4f58139e016",
+    "url": "/react.js-expense-tracker/index.html"
+  },
+  {
+    "revision": "ae8981662a195d989d41",
+    "url": "/react.js-expense-tracker/static/css/main.8db7bfbc.chunk.css"
+  },
+  {
+    "revision": "64629b05b432c44e1bd6",
+    "url": "/react.js-expense-tracker/static/js/2.4599387a.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/react.js-expense-tracker/static/js/2.4599387a.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "ae8981662a195d989d41",
+    "url": "/react.js-expense-tracker/static/js/main.729cb0e1.chunk.js"
+  },
+  {
+    "revision": "3e89a98647f531ba5852",
+    "url": "/react.js-expense-tracker/static/js/runtime-main.08e6fece.js"
+  }
+]);
