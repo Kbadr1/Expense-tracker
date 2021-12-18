9872c2d66078917c66d4dd4b292fc738d8a09852
Kbadr1 <kbadr.dev@gmail.com>
2021-12-18 06:35:53 +0200

    Updates

---
+++ precache-manifest.340847ef9a86ed3ee891fcc03e365430.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c02b025884ce36e7ac5c23330a1ab9e7",
+    "url": "/Expense-tracker/index.html"
+  },
+  {
+    "revision": "e15dae7039e4a884891c",
+    "url": "/Expense-tracker/static/css/main.ae7f934e.chunk.css"
+  },
+  {
+    "revision": "34d96d404102a0c2bfbd",
+    "url": "/Expense-tracker/static/js/2.da3a647b.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/Expense-tracker/static/js/2.da3a647b.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "e15dae7039e4a884891c",
+    "url": "/Expense-tracker/static/js/main.a17e5b49.chunk.js"
+  },
+  {
+    "revision": "abb962dd7c22cd0be0cf",
+    "url": "/Expense-tracker/static/js/runtime-main.82a8dbe3.js"
+  }
+]);
